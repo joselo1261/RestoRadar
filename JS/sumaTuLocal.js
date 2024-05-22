21990e956eb43828e8465b67b7598637c234e62e
Jose Luis Elisseche <joselise61@gmail.com>
2024-05-22 11:27:52 -0300

    Correciones Varias

--- JS/sumaTuLocal.js
+++ JS/sumaTuLocal.js
@@ -78,102 +78,119 @@ cerrar.addEventListener("click", () => {
 
 
 // ----------- Validacion Campos Formulario ----------- //
+
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('contactForm');
+  const imageInput = document.getElementById('imagen');
+  const fileName = document.getElementById('fileName');
 
-  form.addEventListener('submit', (event) => {
-      // Previene el envío del formulario
-      event.preventDefault();
+  // Actualiza el texto de la etiqueta cuando se selecciona un archivo
+  imageInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    fileName.textContent = file ? file.name : 'Una imagen de su Local';
+  });
+    form.addEventListener('submit', (event) => {
+    // Previene el envío del formulario
+    event.preventDefault();
 
-      // Borra mensajes de error previos
-      const errorElements = form.querySelectorAll('.error');
+    // Borra mensajes de error previos
+    const errorElements = form.querySelectorAll('.error');
+    errorElements.forEach(element => element.remove());
+
+    // Validación General
+    let isValid = true;
+
+    // Validación Nombre
+    const nombre = document.getElementById('nombre');
+    if (nombre.value.trim() === '') {
+      showError('nombre', 'El nombre es obligatorio');
+      nombre.value = '';
+      isValid = false;
+    }
+
+    // Validación Apellido
+    const apellido = document.getElementById('apellido');
+    if (apellido.value.trim() === '') {
+      showError('apellido', 'El apellido es obligatorio');
+      apellido.value = '';
+      isValid = false;
+    }
+
+    // Validación Razon Social
+    const razonSocial = document.getElementById('razonSocial');
+    if (razonSocial.value.trim() === '') {
+      showError('razonSocial', 'La razón social es obligatoria');
+      razonSocial.value = '';
+      isValid = false;
+    }
+
+    // Validación Telefono
+    const telefono = document.getElementById('telefono');
+    if (!isValidPhone(telefono.value.trim())) {
+      showError('telefono', 'Introduce un número de teléfono válido (ej. (123)4567890)');
+      telefono.value = '';
+      isValid = false;
+    }
+
+    // Validación Email
+    const email = document.getElementById('email');
+    if (!isValidEmail(email.value.trim())) {
+      showError('email', 'Introduce un correo electrónico válido');
+      email.value = '';
+      isValid = false;
+    }
+
+    // Validación Imagen
+    const file = imageInput.files[0];
+    if (file) {
+      const fileType = file.type;
+    	const validImageTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+      	if (!validImageTypes.includes(fileType)) {
+        	showError('imagen', 'Por favor, sube un archivo de imagen válido (JPEG, JPG, PNG, GIF).');
+        isValid = false;
+      }
+    } else {
+      showError('imagen', 'Por favor, sube una imagen.');
+      isValid = false;
+    }
+
+    // Validación Consulta
+    const consulta = document.getElementById('consulta');
+    if (consulta.value.trim() === '') {
+      showError('consulta', 'La consulta no puede estar vacía');
+      consulta.value = '';
+      isValid = false;
+    }
+    if (isValid) {
       errorElements.forEach(element => element.remove());
-
-      // Validación
-      let isValid = true;
-
-       // Validación Nombre
-       const nombre = document.getElementById('nombre');
-       if (nombre.value.trim() === '') {
-           showError('nombre', 'El nombre es obligatorio');
-           nombre.value = '';
-           isValid = false;
-       }
-
-       // Validación Apellido
-       const apellido = document.getElementById('apellido');
-       if (apellido.value.trim() === '') {
-           showError('apellido', 'El apellido es obligatorio');
-           apellido.value = '';
-           isValid = false;
-       }
-
-       // Validación Razon Social
-       const razonSocial = document.getElementById('razonSocial');
-       if (razonSocial.value.trim() === '') {
-           showError('razonSocial', 'La razón social es obligatoria');
-           razonSocial.value = '';
-           isValid = false;
-       }
-
-       // Validación Telefono
-       const telefono = document.getElementById('telefono');
-       if (!isValidPhone(telefono.value.trim())) {
-        showError('telefono', 'Introduce un número de teléfono válido (ej. (123)4567890)');
-           telefono.value = '';
-           isValid = false;
-       }
-
-       // Validación Email
-       const email = document.getElementById('email');
-       if (!isValidEmail(email.value.trim())) {
-           showError('email', 'Introduce un correo electrónico válido');
-           email.value = '';
-           isValid = false;
-       }
-
-       // Validación Consulta
-       const consulta = document.getElementById('consulta');
-       if (consulta.value.trim() === '') {
-           showError('consulta', 'La consulta no puede estar vacía');
-           consulta.value = '';
-           isValid = false;
-       }
-
-       // Si el formulario es válido, lo envía
-       if (isValid) {
-         // Borra todos los mensajes de error
-         errorElements.forEach(element => element.remove());
-        // Muestra mensaje de enviado
-        alert('Formulario Enviado, gracias por su interés, pronto nos contactaremos');
-        // Resetea formulario para nuevo envío
-        form.reset();
+      // Muestra mensaje de enviado
+      alert('Formulario Enviado, gracias por su interés, pronto nos contactaremos');
+      // Resetea formulario para nuevo envío
+      form.reset();
+      // Restablece el texto de la etiqueta del archivo
+      fileLabel.textContent = 'Una imagen de su Local';
     }
   });
 
   function showError(fieldId, message) {
-      const input = document.getElementById(fieldId);
-      const error = document.createElement('div');
-      error.className = 'error';
-      error.style.color = 'red';
-      error.textContent = message;
-      input.parentElement.appendChild(error);
+    const input = document.getElementById(fieldId);
+    const error = document.createElement('div');
+    error.className = 'error';
+    error.style.color = 'red';
+    error.textContent = message;
+    input.parentElement.appendChild(error);
   }
 
-  function isValidEmail(email) {
-      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return re.test(email);
+  function isValidEmail(email) { // Define una función llamada isValidEmail que toma un argumento email.
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
   }
 
   function isValidPhone(phone) {
     const re = /^\(\d{3,4}\)\d{7,8}$/; // Se valida para el formato (xxx)-xxxxxxxx
     return re.test(phone);
   }
-});		
-
-
-
-
+});
 
 // ------------ Usuarios Login ----------------
 var users = [
@@ -181,7 +198,7 @@ var users = [
   { username: "user1", password: "password1" },
   { username: "marcela", password: "marcela1" },
   { username: "carlos", password: "carlos1" },
-  // Agrega más usuarios aquí si es necesario
+  // Agregar más usuarios aquí si es necesario
 ];
 
 function login() {
@@ -191,7 +208,6 @@ function login() {
 
   if (username === "admin" && password === "admin1") {
     window.location.href = "accesoAdmin1.html";
-    // validUser = true;
     return;
   } else
     for (var i = 1; i < users.length; i++) {
@@ -209,5 +225,3 @@ function login() {
     document.getElementById("password").value = "";
   }
 }
-
-
