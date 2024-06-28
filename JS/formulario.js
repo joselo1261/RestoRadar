1223a9a9b574c51ae1904a3048746655f7e30a81
Jose Luis Elisseche <joselise61@gmail.com>
2024-06-28 17:20:18 -0300

    Actualizaciones menores

--- JS/formulario.js
+++ JS/formulario.js
@@ -73,58 +73,72 @@ cerrar.addEventListener("click", () => {
 });
 
 
+// Script para detalles de reserva y envío de correo
 
-// ---------- ENVIAR FORMULARIO RESERVA ----------
+// Función para validar el formato del correo electrónico
+function validarCorreo(correo) {
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(correo);
+}
 
-// Validación de formulario
-document
-  .getElementById("miFormulario")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+// Recuperar los datos del almacenamiento local
+var restElegido = localStorage.getItem('restElegido');
+var nombre = localStorage.getItem('nombre');
+var telefono = localStorage.getItem('telefono');
+var fecha = localStorage.getItem('fecha');
+var hora = localStorage.getItem('hora');
+var personas = localStorage.getItem('personas');
+var lugar = localStorage.getItem('lugar');
+var mesaReservada = localStorage.getItem('mesaReservada');
 
-    // Validar los datos del formulario
-    if (validarFormulario()) {
-      // Enviar formulario si es válido
-      enviarFormulario();
-    } else {
-      // Mostrar mensaje de error al usuario si el formulario no es válido
-      mostrarMensajeError();
-    }
+// Mostrar los datos en la página
+document.getElementById('restElegido').textContent = restElegido;
+document.getElementById('nombre').textContent = nombre;
+document.getElementById('telefono').textContent = telefono;
+document.getElementById('fecha').textContent = fecha;
+document.getElementById('hora').textContent = hora;
+document.getElementById('personas').textContent = personas;
+document.getElementById('lugar').textContent = lugar;
+document.getElementById('mesaReservada').textContent = mesaReservada;
+
+// Agregar evento clic al botón de enviar correo
+document.getElementById('enviarCorreo').addEventListener('click', function() {
+  var correo = document.getElementById('email').value;
+  if (validarCorreo(correo)) {
+    // Aquí puedes agregar el código para enviar el correo
+    alert('Correo enviado\nGracias por Elegirnos\nLos esperamos !!!!');
+  } else {
+    alert('Por favor, introduce un correo electrónico válido\nFormato: xxxxxx@xxx.xxx')
+  }
+});
+
+// Validación de formulario y envío de datos
+document.getElementById("miFormulario").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  // Obtener los datos del formulario utilizando FormData
+  const formData = new FormData(form);
+  const restElegido = document.getElementById("restElegido").textContent;
+  const mesaReservada = document.getElementById("numeroSeleccionado").textContent;
+
+  const datos = {
+    restElegido,
+    nombre: formData.get("nombre"),
+    telefono: formData.get("telefono"),
+    fecha: new Date(formData.get("fecha")).toLocaleDateString("es-ES"),
+    hora: formData.get("hora"),
+    personas: formData.get("personas"),
+    lugar: formData.get("lugar"),
+    mesaReservada
+  };
+
+  // Guardar los datos en el almacenamiento local
+  Object.entries(datos).forEach(([key, value]) => {
+    localStorage.setItem(key, value);
   });
 
-
-// Obtener los datos del formulario y enviar correo después de la redirección
-document
-  .getElementById("miFormulario")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const form = event.target;
-
-    // Obtener los datos del formulario utilizando FormData
-    const formData = new FormData(form);
-    const restElegido = document.getElementById("restElegido").textContent;
-    const mesaReservada = document.getElementById("numeroSeleccionado").textContent;
-
-    const datos = {
-      restElegido,
-      nombre: formData.get("nombre"),
-      telefono: formData.get("telefono"),
-      fecha: new Date(formData.get("fecha")).toLocaleDateString("es-ES"),
-      hora: formData.get("hora"),
-      personas: formData.get("personas"),
-      lugar: formData.get("lugar"),
-      mesaReservada
-    };
-
-    // Guardar los datos en el almacenamiento local
-    Object.entries(datos).forEach(([key, value]) => {
-      localStorage.setItem(key, value);
-    });
-
-    // Redirigir a la página de destino
-    window.location.href = "./formulario.html";
-
-    // Enviar el correo electrónico después de la redirección
-    enviarCorreo();
-  });
+  // Redirigir a la página de destino
+  window.location.href = "./formulario.html";
+});
